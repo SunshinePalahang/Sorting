e15edc44470c825209979d5490288b2c386d5e49
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2023-01-23 10:30:29 +0800

    setting temporary value and sorting from ascending order

--- selection_sort.py
+++ selection_sort.py
@@ -7,6 +7,11 @@ def sort(nums):
             if nums[j] < nums[min]:
                 min = j
 
+        temp = nums[i]
+        nums[i] = nums[min]
+        nums[min] = temp
+
+
 nums = [59, 90, 96, 12, 28, 34, 88, 13, 48, 77]
 sort(nums)
 
