8bba093fce05f0f23a26ec6e394be43fc20a206e
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2023-01-23 12:12:26 +0800

    defining partition, to return the index of the pivot

--- quick_sort.py
+++ quick_sort.py
@@ -2,7 +2,14 @@
 def sort(nums, left, right):
     if left < right:
         partition_pos = partition(nums, left, right)
+        sort(nums, left, partition_pos - 1)
+        sort(nums, partition_pos + 1, right)
 
+def partition(nums, left, right):
+    i = left
+    j = right
+    pivot = nums[right]
+    
 
 nums = [59, 90, 96, 12, 28, 34, 88, 13, 48, 77]
 sort(nums)
