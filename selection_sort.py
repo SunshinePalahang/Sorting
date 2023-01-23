67ca6d7bdfd38396e732ef2cc54aa0039c231d74
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2023-01-23 10:27:57 +0800

    finding the least number

--- selection_sort.py
+++ selection_sort.py
@@ -3,6 +3,11 @@
 def sort(nums):
     for i in range(9):
         min = i
+        for j in range (i,10):
+            if nums[j] < nums[min]:
+                min = j
 
 nums = [59, 90, 96, 12, 28, 34, 88, 13, 48, 77]
 sort(nums)
+
+print (nums)
