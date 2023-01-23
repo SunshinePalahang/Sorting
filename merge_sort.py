08a68edf331321412d54e1fd98719b1ce5c1d6ef
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2023-01-23 11:42:16 +0800

    defining sort(nums)

--- merge_sort.py
+++ merge_sort.py
@@ -1,5 +1,8 @@
 # 59, 90, 96, 12, 28, 34, 88, 13, 48, 77
-
+def sort(nums):
+    if len(nums)>1:
+        left_nums = nums[:len(nums)//2]
+        right_nums = nums[len(nums)//2:]
 
 nums = [59, 90, 96, 12, 28, 34, 88, 13, 48, 77]
 sort(nums)
