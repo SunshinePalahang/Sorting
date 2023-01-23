561de0ea5c9096c6474bfd144693b27ee0cb72c7
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2023-01-23 11:51:34 +0800

    Comparing and swapping values

--- merge_sort.py
+++ merge_sort.py
@@ -4,6 +4,32 @@ def sort(nums):
         left_nums = nums[:len(nums)//2]
         right_nums = nums[len(nums)//2:]
 
+        sort(left_nums)
+        sort(right_nums)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_nums) and j < len(right_nums):
+            if left_nums[i] < right_nums[i]:
+                nums[k] = left_nums[i]
+                i += 1
+                k += 1
+            else:
+                nums[k] = right_nums[j]
+                j += 1
+                k += 1
+
+        while i < len(left_nums):
+            nums[k] = left_nums[i]
+            i += 1
+            k += 1
+
+        while j < len(right_nums):
+            nums[k] = right_nums[j]
+            j += 1
+            k += 1
+
 nums = [59, 90, 96, 12, 28, 34, 88, 13, 48, 77]
 sort(nums)
 
