ca6c947257aa087e355cce949320ad84cf58a37d
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2023-01-23 10:47:36 +0800

    sorting outer loop

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,12 @@
 
 def sort(nums):
     for i in range(len(nums)-1,0,-1):
-        
+        for j in range(i):
+            if nums[j] > nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j + 1]
+                nums[j+1] = temp
+
 
 nums = [59, 90, 96, 12, 28, 34, 88, 13, 48, 77]
 sort(nums)
