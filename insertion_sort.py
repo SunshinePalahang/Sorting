bfb1e7de946ea8d394e26cd81a792cdf4d807279
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2023-01-23 11:16:42 +0800

    comparing and swapping values

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,8 +1,17 @@
 # 59, 90, 96, 12, 28, 34, 88, 13, 48, 77
 def sort(nums):
-    for i in range(1, len(nums)):
-        min = nums[i]
-        
+    for index in range(1, len(nums)):
+        min = nums[index]
+        i = index - 1
+        while i >= 0:
+            if min < nums[i]:
+                nums[i+1] = nums[i]
+                nums[i] = min
+                i = i - 1
+            
+            else:
+                break
+
 
 nums = [59, 90, 96, 12, 28, 34, 88, 13, 48, 77]
 sort(nums)
