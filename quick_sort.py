9af1adb0626c356a356c9984eb5805fc97fe18c1
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2023-01-23 12:08:02 +0800

    defining sort nums

--- quick_sort.py
+++ quick_sort.py
@@ -1,4 +1,7 @@
 # 59, 90, 96, 12, 28, 34, 88, 13, 48, 77
+def sort(nums, left, right):
+    if left < right:
+        partition_pos = partition(nums, left, right)
 
 
 nums = [59, 90, 96, 12, 28, 34, 88, 13, 48, 77]
